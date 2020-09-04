f9b5bc5544bf9350586de2f2916c965ebaad28da
Saurabh Daware <saurabhdaware99@gmail.com>
2020-09-04 16:14:43 +0530

    Add dev environment to test

--- abell.config.js
+++ abell.config.js
@@ -1,9 +1,29 @@
 // Don't change any of these values as they have to be this way for action to run
+
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const isDevEnv = process.env.NODE_ENV === "dev";
+const rootPath = isDevEnv ? "" : "..";
+
+let githubData = {};
+
+if (fs.existsSync("./githubData.json")) {
+  githubData = require("./githubData.json");
+}
+
+let userGlobalMeta = {};
+if (fs.existsSync(path.join(rootPath, "globalMeta.json"))) {
+  userGlobalMeta = require(path.join(rootPath, "globalMeta.json"));
+}
+
 module.exports = {
-  contentPath: "theme",
-  themePath: "theme",
+  contentPath: "layout",
+  themePath: "layout",
   outputPath: "../docs",
   globalMeta: {
-    userReadmeURL: "../../README.md"
+    userReadmeURL: isDevEnv ? "DEV_README.md" : "../../README.md",
+    github: githubData,
+    ...userGlobalMeta
   }
 };
